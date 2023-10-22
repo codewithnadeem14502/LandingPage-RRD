12610f347e2a96143626de7bc90192e12691d423
MohdNadeem <codewithnadeem@gmail.com>
2023-10-23 00:39:43 +0530

    started project

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Header from "./Header";
+import Footer from "./Footer";
+import Github from "./Github";
+
+export { Home, Header, About, Contact, Github, Footer };
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import { About, Contact, Github, Home } from "./components";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="about" element={<About />} />
+      <Route path="github" element={<Github />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
